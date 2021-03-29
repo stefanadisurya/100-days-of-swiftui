f3f69d6813a9df501d577d66e3093e6816f787eb
Stefan Adisurya <stefanadisurya@gmail.com>
2021-03-29 20:50:34 +0700

    commit day 12

---
+++ Day 12/Optionals.playground/Contents.swift
@@ -0,0 +1,107 @@
+import UIKit
+
+// Handling missing data
+var age: Int?
+var number: Int? = nil
+number = 38
+
+// Unwrapping optionals
+var name: String? = nil
+
+if let unwrapped = name {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
+print()
+
+// Unwrapping with guard
+func greet(_ name: String?) {
+    guard let unwrapped = name else {
+        print("You didn't provide a name!")
+        return
+    }
+    
+    print("Hello, \(unwrapped)!")
+}
+
+greet(nil)
+greet("Stefan")
+print()
+
+// Force unwrapping
+let str = "5"
+let num = Int(str)!
+
+// Implicitly unwrapped optionals -> doesn't need if let or gurad let
+let age2: Int! = nil
+
+// Nil coalescing
+func username(for id: Int) -> String? {
+    if id == 1 {
+        return "Taylor Swift"
+    } else {
+        return nil
+    }
+}
+
+let user = username(for: 15) ?? "Anonymous"
+print(user)
+print()
+// Optional chaining -> the rest of after the optional value will be ignored the optional have no value
+let names = ["John", "Paul", "George", "Ringo"]
+let beatle = names.first?.uppercased()
+
+// Optional try
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+if let result = try? checkPassword("password") {
+    print("Result was \(result)")
+} else {
+    print("D'oh.")
+}
+
+try! checkPassword("sekrit")
+print("OK!")
+print()
+
+// Failable initializers
+struct Person {
+    var id: String
+    
+    init?(id: String) {
+        if id.count == 9 {
+            self.id = id
+        } else {
+            return nil
+        }
+    }
+}
+
+// Typecasting
+class Animal { }
+class Fish: Animal { }
+
+class Dog: Animal {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+let pets = [Fish(), Dog(), Fish(), Dog()]
+
+for pet in pets {
+    if let dog = pet as? Dog {
+        dog.makeNoise()
+    }
+}
