e3873cdd3d024ee8f6a4382f3b798536b4b25f11
Stefan Adisurya <stefanadisurya@gmail.com>
2021-04-07 08:02:15 +0700

    commit day 21

--- Day 20 22/Project/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day 20 22/Project/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,28 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0 ... 2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
     
     var body: some View {
-//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-        
-//        RadialGradient(gradient: Gradient(colors: [Color.red, Color.blue]), center: .center, startRadius: 20, endRadius: 200)
-        
-//        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-        
-//        Button(action: {print("Button was tapped")}) {
-//            HStack(spacing: 10) {
-//                Image(systemName: "pencil")
-//                Text("Edit")
-//            }
-//        }
-        
-        Button("Show Alert") {
-            self.showingAlert = true
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                        .font(.title)
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 300, height: 150, alignment: .center)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Spacer()
+            }
         }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Hello SwiftUI"), message: Text("This is some detail mesage"), dismissButton: .default(Text("OK")))
-        }
+        .alert(isPresented: $showingScore, content: {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        })
     }
 }
 
