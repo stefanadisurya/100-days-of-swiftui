18bc648703f564d860b15c3e7c5c6622c9f7a535
Stefan Adisurya <stefanadisurya@gmail.com>
2021-04-18 19:36:25 +0700

    commit day 32

---
+++ Day 32 34/Animation/Animation/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Stefan Adisurya on 18/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0, y: 1, z: 0)
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
