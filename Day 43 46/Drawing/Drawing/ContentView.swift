003a11787c452b8f6ab6c455f11cca767e8c366b
Stefan Adisurya <stefanadisurya@gmail.com>
2021-05-01 08:53:52 +0700

    commi day 45

--- Day 43 46/Drawing/Drawing/ContentView.swift
+++ Day 43 46/Drawing/Drawing/ContentView.swift
@@ -7,47 +7,56 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-//                    .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
             }
         }
-        .drawingGroup()
-    }
-
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+        
+        return path
     }
 }
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
-
-        var body: some View {
-            VStack {
-                ColorCyclingCircle(amount: self.colorCycle)
-                    .frame(width: 300, height: 300)
-
-                Slider(value: $colorCycle)
+    @State private var rows = 4
+    @State private var columns = 4
+    
+    var body: some View {
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
+                }
             }
-        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
