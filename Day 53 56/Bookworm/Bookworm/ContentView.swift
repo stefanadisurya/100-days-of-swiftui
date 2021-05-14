dfe701379b25ac274a328ca035da8a3ca5668d05
Stefan Adisurya <stefanadisurya@gmail.com>
2021-05-14 11:37:35 +0700

    commit day 53

---
+++ Day 53 56/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Stefan Adisurya on 14/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    @Environment(\.managedObjectContext) var moc
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(students, id: \.id) { student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: self.moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? self.moc.save()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
